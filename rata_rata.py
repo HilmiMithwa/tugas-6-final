6f2bf8c6a4158be06b36f1ecab948f83a84c4ac4
HilmiMithwa <hilmimithwa@gmail.com>
2024-11-11 20:12:37 +0700

    Inisialisasi commit

---
+++ rata_rata.py
@@ -0,0 +1,15 @@
+#NAMA: Hilmi Mithwa Ramadhan
+#KELAS: RPL-1C
+#NIM 2404042
+
+#RATA-RATA
+
+def average(*angka):
+    total = sum(angka)
+
+    rata_rata = total / len(angka)
+
+    print("Rata-rata", rata_rata)
+
+
+average(5,10)
